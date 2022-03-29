4f1eaf3881a0e1d8aa1911295c99379947bcb91e
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2022-03-29 22:47:04 +0300

    4.2. Результат работы метода [#232622] fix

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -6,11 +6,6 @@ public class ArgMethod {
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
-    public static int func1(int x) {
-        int y = x * x + 1;
-        return y;
-    }
-
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
@@ -18,7 +13,5 @@ public class ArgMethod {
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
-        int result3 = ArgMethod.func1(100);
-        System.out.println(result3);
     }
 }
