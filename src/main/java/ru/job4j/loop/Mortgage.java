65f5b556390258d845275eafba046f22b9eebb91
GrizzlLearn <dylebedev.learning@gmail.com>
2022-04-11 22:38:26 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        double am = amount;
+
+        while (am > 0) {
+            am += am * (percent / 100) - salary;
+            year++;
+        }
+        return year;
+    }
+}
